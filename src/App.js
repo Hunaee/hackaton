0866ca254d34f6a4f333d4a485fb26736381e56e
Hunaee <67273688+Hunaee@users.noreply.github.com>
2025-02-04 12:41:58 +0100

    1er commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Contact from './pages/contact'
+
+
+
+function App() {
+  return (
+     
+    <Contact />
+
+  );
+}
+
+export default App;
---
+++ src/pages/contact.js
@@ -0,0 +1,13 @@
+
+
+const Contact = () => {
+
+    const string = "Hello World"
+
+    return(
+        <h1 className="margin-sm">{string}</h1>
+    )
+}
+
+
+export default Contact
